794c9544abe33283d2debef5a7915638a6330e9f
abdellahi-brahim <79083991+abdellahi-brahim@users.noreply.github.com>
2023-09-26 14:36:58 +0100

    Update install-gcc.sh

--- install-gcc.sh
+++ install-gcc.sh
@@ -3,31 +3,35 @@
 # Define the repository URL and the tarball name
 REPO_URL="https://github.com/abdellahi-brahim/gcc.git"
 TARBALL_NAME="gcc-10.1.0.tar.gz"
-DESTINATION_PATH="${HOME}/opt"
+DESTINATION_PATH="/opt"
 
-# Install git
-if command -v apt-get &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get install -y git
-elif command -v yum &> /dev/null; then
-    sudo yum install -y git
-else
-    echo "Unsupported package manager. Please install git manually."
-    exit 1
+# Install git if not already installed
+if ! command -v git &> /dev/null; then
+    if command -v apt-get &> /dev/null; then
+        sudo apt-get update
+        sudo apt-get install -y git
+    elif command -v yum &> /dev/null; then
+        sudo yum install -y git
+    else
+        echo "Unsupported package manager. Please install git manually."
+        exit 1
+    fi
 fi
 
-# Install git-lfs
-if command -v apt-get &> /dev/null; then
-    # For apt-based systems (like Ubuntu and Debian)
-    curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-    sudo apt-get install -y git-lfs
-elif command -v yum &> /dev/null; then
-    # For yum-based systems (like CentOS and RHEL)
-    curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo bash
-    sudo yum install -y git-lfs
-else
-    echo "Unsupported package manager. Please install git-lfs manually."
-    exit 1
+# Install git-lfs if not already installed
+if ! command -v git-lfs &> /dev/null; then
+    if command -v apt-get &> /dev/null; then
+        # For apt-based systems (like Ubuntu and Debian)
+        curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+        sudo apt-get install -y git-lfs
+    elif command -v yum &> /dev/null; then
+        # For yum-based systems (like CentOS and RHEL)
+        curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo bash
+        sudo yum install -y git-lfs
+    else
+        echo "Unsupported package manager. Please install git-lfs manually."
+        exit 1
+    fi
 fi
 
 # Clone the repository
@@ -38,7 +42,7 @@ cd gcc_repo
 git lfs pull
 
 # Extract the tarball
-tar xzf "$TARBALL_NAME" -C "$DESTINATION_PATH"
+sudo tar xzf "$TARBALL_NAME" -C "$DESTINATION_PATH"
 
 # Activate the new GCC
 source "${DESTINATION_PATH}/gcc-10.1.0/activate"
