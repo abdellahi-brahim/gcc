c8f9f9678a720dc260af8ef453fdc08a6008ffd7
abdellahi-brahim <79083991+abdellahi-brahim@users.noreply.github.com>
2023-09-26 14:30:34 +0100

    Update install-gcc.sh

--- install-gcc.sh
+++ install-gcc.sh
@@ -1,38 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
 # Define the repository URL and the tarball name
 REPO_URL="https://github.com/abdellahi-brahim/gcc.git"
 TARBALL_NAME="gcc-10.1.0.tar.gz"
 DESTINATION_PATH="${HOME}/opt"
 
-# Function to extract tarball based on its file type
-extract_tarball() {
-    case $1 in
-        *.tar.bz2) 
-            tar xjf $1 -C "$DESTINATION_PATH" && return 0 ;;
-        *.tar.gz) 
-            tar xzf $1 -C "$DESTINATION_PATH" && return 0 ;;
-        *.tar.xz) 
-            tar xJf $1 -C "$DESTINATION_PATH" && return 0 ;;
-        *.tar) 
-            tar xf $1 -C "$DESTINATION_PATH" && return 0 ;;
-        *) 
-            echo "Unknown tarball type: $1"; exit 1 ;;
-    esac
-}
+# Install git
+if command -v apt-get &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y git
+elif command -v yum &> /dev/null; then
+    sudo yum install -y git
+else
+    echo "Unsupported package manager. Please install git manually."
+    exit 1
+fi
+
+# Install git-lfs
+if command -v apt-get &> /dev/null; then
+    # For apt-based systems (like Ubuntu and Debian)
+    curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+    sudo apt-get install -y git-lfs
+elif command -v yum &> /dev/null; then
+    # For yum-based systems (like CentOS and RHEL)
+    curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo bash
+    sudo yum install -y git-lfs
+else
+    echo "Unsupported package manager. Please install git-lfs manually."
+    exit 1
+fi
 
 # Clone the repository
 git clone "$REPO_URL" gcc_repo
 cd gcc_repo
 
-# Check if the tarball exists
-if [ ! -f "$TARBALL_NAME" ]; then
-    echo "Error: Tarball $TARBALL_NAME not found in the repository."
-    exit 1
-fi
+# Pull the LFS content
+git lfs pull
 
 # Extract the tarball
-extract_tarball "$TARBALL_NAME"
+tar xzf "$TARBALL_NAME" -C "$DESTINATION_PATH"
 
 # Activate the new GCC
 source "${DESTINATION_PATH}/gcc-10.1.0/activate"
