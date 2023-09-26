bd8eadcbf3e263da9931610e3dd6878a2a9e587f
abdellahi <uchihaabdellahi@gmail.com>
2023-09-26 10:30:10 +0000

    first commit

---
+++ gcc-10.1.0/lib/gcc/x86_64-unknown-linux-gnu/10.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/var/tmp/root/gcc-10.1.0_source/gcc-10.1.0/configure --prefix=/root/opt/gcc-10.1.0 --enable-bootstrap --enable-shared --enable-threads=posix --enable-checking=release --with-system-zlib --enable-__cxa_atexit --disable-libunwind-exceptions --enable-linker-build-id --enable-languages=c,c++,lto --disable-vtable-verify --with-default-libstdcxx-abi=new --enable-libstdcxx-debug --without-included-gettext --enable-plugin --disable-initfini-array --disable-libgcj --enable-plugin --disable-multilib --with-tune=generic --build=x86_64-unknown-linux-gnu --target=x86_64-unknown-linux-gnu --host=x86_64-unknown-linux-gnu --with-pkgversion=root-centos-s-4vcpu-8gb-amd-ams3-01";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" }, { "tune", "generic" } };
