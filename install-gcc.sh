073b54791402d569da3e9b651d4044de2fce0d07
abdellahi-brahim <79083991+abdellahi-brahim@users.noreply.github.com>
2023-09-26 14:54:21 +0100

    Update install-gcc.sh

--- install-gcc.sh
+++ install-gcc.sh
@@ -44,6 +44,8 @@ git lfs pull
 # Extract the tarball
 sudo tar xzf "$TARBALL_NAME" -C "$DESTINATION_PATH"
 
+export PATH="${DESTINATION_PATH}/gcc-10.1.0/bin:$PATH"
+
 # Source the activate script to set up the environment for the new GCC
 source "${DESTINATION_PATH}/gcc-10.1.0/activate"
 
